b196705543001b0e8adbc4028f2fd6d7f530523c
felipegalina <65051987+felipegalina@users.noreply.github.com>
2021-04-27 19:11:40 -0300

    Update baseURL.js

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,17 +1,19 @@
-import axios from "axios";
+import axios from "axios"
+
 const instance = axios.create({
-  baseURL: "https://api.api-futebol.com.br/v1",
+  baseURL: "https://api.api-futebol.com.br/v1/",
 });
 instance.interceptors.request.use(
   async (config) => {
-    config.headers.Authorization = `Bearer ${process.env.TOKEN}`;
+    config.Authorization = {headers: {'Authorization': `Bearer live_bbea34333c21ec7d941634016845c7`}};
 
     return config;
   },
-  (err) => {
-    console.log(err);
-    return Promise.reject(err);
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
   }
 );
 
 export default instance;
+
